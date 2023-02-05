2bd890a31a302aad7076b94a7013c96a551c52a5
ryan-rutishauser <98336166+ryan-rutishauser@users.noreply.github.com>
2023-02-05 15:53:42 -0500

    install & start npm

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
-// Copy `img/` to `_site/img`
+  // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addWatchTarget("./src/**/*.html");
